66b61bfba1de1d97a427dd8ec5f62c22fa1d9fe2
Rosmar10 <94025464+Rosmar10@users.noreply.github.com>
2022-06-28 23:07:21 -0400

    styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,11 @@ container.style.backgroundColor = "#7ba238"
 container.style.width = "30%"
 container.style.borderRadius = "3%"
 container.style.margin = "10%"
+container.style.padding = "4%"
 
 
-colorselector = document.getElementById("colorselector")
-colorselector = style.height = "5%"
-colorselector = style.wigth = "10%"
-colorselector = style.backgroundColor = "#000"
-colorselector = style.borderRadius = "50%"
+colorfinal = document.getElementById("colorfinal")
+colorfinal = style.height = "5%"
+colorfinal = style.wigth = "10%"
+colorfinal = style.backgroundColor = "#000"
+colorfinal = style.borderRadius = "50%"
